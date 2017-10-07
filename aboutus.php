fa1142a937f179c1b4dd9b9a94c959551446196a
afriliapink <afriliapink@outlook.com>
2017-10-07 14:38:39 +0700

    tinggal tubes, photo, sosmed

---
+++ aboutus.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AFRILIA</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- using bootstrap 3.3.7 -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Lobster|Ubuntu+Condensed" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+		<!-- style from owner -->
+		<link rel="stylesheet" type="text/css" href="css/style_profile.css">
+		<body>
+			<header>
+				<a class="back" href="index.html">MEDIAGI</a>
+			</header>
+			<div class="bio">
+				<nav class="nav">
+					<img src="img/bomi.jpg" alt="Mountain View" style="width:500px;height:250px;">
+				</nav>
+				<article class="article">
+					<h1 class="nama">Afrilia</h1>
+					<p>London is the capital city of England. It is the most populous city in the United Kingdom,
+					with a metropolitan area of over 13 million inhabitants.</p>
+					<p>Standing on the River Thames, London has been a major settlement for two millennia,
+					its history going back to its founding by the Romans, who named it Londinium.</p>
+					<p><strong>Resize this page to see that what happens!</strong></p>
+					<a class="link" href="https://www.twitter.com/harlnim/"><i class="fa fa-twitter"></i></a>
+					<a class="link" href="https://www.instagram.com/leaakizary/"><i class="fa fa-instagram"></i></a>
+					<a class="link" href="https://www.facebook.com/bomineungwiyomi/"><i class="fa fa-facebook"></i></a>
+				</article>
+			</article>
+		</div>
+		<div class="bio">
+			<nav class="nav">
+				<img src="img/bomi.jpg" alt="Mountain View" style="width:500px;height:250px;">
+			</nav>
+			<article class="article">
+				<h1 class="nama">Melani Puspa Sari</h1>
+				<p>London is the capital city of England. It is the most populous city in the United Kingdom,
+				with a metropolitan area of over 13 million inhabitants.</p>
+				<p>Standing on the River Thames, London has been a major settlement for two millennia,
+				its history going back to its founding by the Romans, who named it Londinium.</p>
+				<p><strong>Resize this page to see that what happens!</strong></p>
+				<a class="link" href="https://www.twitter.com/harlnim/"><i class="fa fa-twitter"></i></a>
+				<a class="link" href="https://www.instagram.com/bomineungwiyomi/"><i class="fa fa-instagram"></i></a>
+				<a class="link" href="https://www.facebook.com/melani.p.sari.9/"><i class="fa fa-facebook"></i></a>
+			</article>
+		</div>
+		<div class="bio">
+			<nav class="nav">
+				<img src="img/bomi.jpg" alt="Mountain View" style="width:500px;height:250px;">
+			</nav>
+			<article class="article">
+				<h1 class="nama">Nadiya Fahada</h1>
+				<p>London is the capital city of England. It is the most populous city in the United Kingdom,
+				with a metropolitan area of over 13 million inhabitants.</p>
+				<p>Standing on the River Thames, London has been a major settlement for two millennia,
+				its history going back to its founding by the Romans, who named it Londinium.</p>
+				<p><strong>Resize this page to see that what happens!</strong></p>
+				<a class="link" href="https://www.twitter.com//"><i class="fa fa-twitter"></i></a>
+				<a class="link" href="https://www.instagram.com/leaakizary/"><i class="fa fa-instagram"></i></a>
+				<a class="link" href="https://www.facebook.com/afriliapink/"><i class="fa fa-facebook"></i></a>
+			</article>
+		</article>
+	</div>
+	<div class="bio">
+		<nav class="nav">
+			<img src="img/bomi.jpg" alt="Mountain View" style="width:500px;height:250px;">
+		</nav>
+		<article class="article">
+			<h1 class="nama">Anggi Juwita</h1>
+			<p>London is the capital city of England. It is the most populous city in the United Kingdom,
+			with a metropolitan area of over 13 million inhabitants.</p>
+			<p>Standing on the River Thames, London has been a major settlement for two millennia,
+			its history going back to its founding by the Romans, who named it Londinium.</p>
+			<p><strong>Resize this page to see that what happens!</strong></p>
+			<a class="link" href="https://www.twitter.com/harlnim/"><i class="fa fa-twitter"></i></a>
+			<a class="link" href="https://www.instagram.com/leaakizary/"><i class="fa fa-instagram"></i></a>
+			<a class="link" href="https://www.facebook.com/afriliapink/"><i class="fa fa-facebook"></i></a>
+		</article>
+	</article>
+</div>
+<div>
+	<a class="btn btn-primary" id="btnpp" href="index.html">home</a>
+</div>
+<div>
+<footer>Copyright &copy; mediagi</footer> </div>
+</div>
+</div>
+</body>
+</html>
